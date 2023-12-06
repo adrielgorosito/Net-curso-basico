14b32cb6387ab136680272e4c78ba5b5901324d2
Adriel Gorosito <adrielgorosito14@gmail.com>
2023-12-06 15:30:34 -0300

    Primeros pasos

---
+++ Proyecto_HolaMundo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Proyecto_HolaMundo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+            Console.ReadKey();
+        }
+    }
+}
