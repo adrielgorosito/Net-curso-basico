7a77d486da10df7034fa367d8e3ef10684cc7c1b
Adriel Gorosito <adrielgorosito14@gmail.com>
2023-12-06 16:59:56 -0300

    Iteraciones while, do while y for

---
+++ Proyecto_Iteraciones/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Proyecto_Iteraciones
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // While
+            int i = 1;
+
+            while(i <= 5)
+            {
+                Console.WriteLine("While: " + i);
+                i++;
+            }
+
+            i = 1;
+
+            // Do while
+            do
+            {
+                Console.WriteLine("Do while: " + i);
+            } while (i <= 0);
+
+            // For
+            for (i = 11; i < 16; i++)
+            {
+                Console.WriteLine("For:" + i);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
