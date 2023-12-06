7fd4cdaad5d82013e07789f238d9f347549e8b04
Adriel Gorosito <adrielgorosito14@gmail.com>
2023-12-06 15:32:46 -0300

    Operadores relacionales y lógicos

---
+++ Proyecto_OperadoresRelacionales/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Proyecto_OperadoresRelacionales
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 10, num2 = 5;
+            bool resultado;
+
+            // Operadores relacionales:
+            // >, <, ==, >=, <=, !=
+            resultado = num1 > num2;
+            resultado = num1 != num2;
+
+            Console.WriteLine("Num1 es mayor a num2? " + (num1 > num2));
+
+            // Operadores lógicos
+            // &&, ||, !
+            resultado = (num1 > num2) && (num1 == 0);
+            resultado = !(num1 == num2);
+
+            Console.ReadKey();
+        }
+    }
+}
