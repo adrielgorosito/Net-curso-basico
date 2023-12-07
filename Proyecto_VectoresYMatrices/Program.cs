8f33e5966e5b278e91647d694852a7e334707d8c
Adriel Gorosito <adrielgorosito14@gmail.com>
2023-12-07 10:25:02 -0300

    Vectores y matrices

---
+++ Proyecto_VectoresYMatrices/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Proyecto_VectoresYMatrices
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] vectorNombres = { "Victor", "Carlos", "Ana", "Luis", "Jorge" };
+
+            Console.WriteLine("Nombre: " + vectorNombres[2]);
+
+            Console.WriteLine("\nIteración con foreach:");
+            foreach (string n in vectorNombres)
+            {
+                Console.WriteLine(n);
+            }
+            
+
+            string[,] matrizAnimal = new string[3, 2];
+
+            matrizAnimal[0, 0] = "Gato";
+            matrizAnimal[0, 1] = "Perro";
+            matrizAnimal[1, 0] = "Pájaro";
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    if (matrizAnimal[i, j] != null)
+                        Console.WriteLine("Fila: " + i + " | Animal: " + matrizAnimal[i, j]);
+                    else
+                        Console.WriteLine("Fila: " + i + " | ...");
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
