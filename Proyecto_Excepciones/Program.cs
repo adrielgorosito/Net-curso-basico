1049c84ab2afddb886f302c316382dbd0d6ba14e
Adriel Gorosito <adrielgorosito14@gmail.com>
2023-12-08 00:11:46 -0300

    Manejo de excepciones

---
+++ Proyecto_Excepciones/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Excepciones
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingresa tu edad: ");
+            try 
+            { 
+                int edad = int.Parse(Console.ReadLine());
+                Console.WriteLine("Tu edad es: " + edad);
+            } catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            } finally
+            {
+                Console.ReadKey();
+            }
+        }
+    }
+}
