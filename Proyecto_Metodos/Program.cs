37239d4dd85cf9ed47b0fef11379919d1dab23be
Adriel Gorosito <adrielgorosito14@gmail.com>
2023-12-08 00:05:59 -0300

    Métodos

---
+++ Proyecto_Metodos/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Proyecto_Metodos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Función 1: ");
+            ingresoDatos();
+
+            Console.WriteLine("\n\nFunción 2:\nIngrese dos números: ");
+
+            Console.WriteLine("Suma: " + sum(Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine())));
+
+            Console.ReadKey();
+        }
+
+        // Primera función
+        static void ingresoDatos()
+        {
+            string nombre;
+            Console.WriteLine("Ingrese el nombre:");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Nombre: " + nombre);
+
+        } 
+
+        // Función con parámetro
+        static int sum(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+    }
+}
